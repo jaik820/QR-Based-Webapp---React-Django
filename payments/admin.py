f3088feb7c2154c34cf8610a8a2b07fa872e5873
Jaikumar <30980606+jaik820@users.noreply.github.com>
2025-06-04 13:46:53 +0400

    Delete payments directory

--- payments/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Payment
-
-@admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'merchant_name', 'amount', 'status')
-    search_fields = ('merchant_name', 'id')
